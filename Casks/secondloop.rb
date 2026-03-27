5faf8835de176cab0a54e38c7fa63e72628fa056
dale0525 <dale0525@users.noreply.github.com>
2026-03-27 05:25:54 +0000

    chore(cask): update secondloop v1.27.0

--- Casks/secondloop.rb
+++ Casks/secondloop.rb
@@ -1,8 +1,8 @@
 cask "secondloop" do
-  version "1.26.2"
-  sha256 "4f1290dc922b80c7d7ac5a41250e8e52f2649a5356054cdf48f81cf74f98a7a6"
+  version "1.27.0"
+  sha256 "7cf8a73597b74ec492b3bf525244077b2488485f27bcd367ee2fcb64d7f267a6"
 
-  url "https://github.com/dale0525/SecondLoop/releases/download/v1.26.2/SecondLoop-macos-v1.26.2.dmg",
+  url "https://github.com/dale0525/SecondLoop/releases/download/v1.27.0/SecondLoop-macos-v1.27.0.dmg",
       verified: "github.com/dale0525/SecondLoop/"
   name "SecondLoop"
   desc "Local-first personal AI assistant with long-term memory"
